6c644633c3a18fcdd91d4d595dacf0b40b24da65
thalewnet <lewgasaemsant.t@gmail.com>
2021-10-20 16:52:20 +0700

    'dropdown'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import PostImg from './components/Post/PostImg';
 import PostText from './components/Post/PostText';
-import Header from "./components/Header";
-import Login from "./components/Login";
+import Header from './components/Header';
+import Login from './components/Login';
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Login />
       <Switch>
+        <Route path="login" component={Login} />
         <Route path="/5" component={PostImg} />
         <Route path="/" component={PostText} />
       </Switch>
--- src/components/Post/Dropdown.js
+++ src/components/Post/Dropdown.js
@@ -1,37 +1,72 @@
-import React from 'react';
-import { HiChevronDown } from 'react-icons/hi';
+import React, { useState } from 'react';
+import { HiChevronDown, HiOutlineUserCircle } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
 import DropdownCard from './DropdownCard';
 
 const MOCK_DATA = [
-  { name: ' r/javascript', members: '10,000' },
-  { name: ' r/dota2', members: '100,000' },
-  { name: ' r/css', members: '9,000' },
-  { name: ' r/tailwind', members: '12,000' },
-  { name: ' r/tailwind', members: '12,000' },
+  {
+    name: ' r/javascript',
+    members: '10,000',
+    imgUrl: 'https://randomuser.me/api/portraits/men/85.jpg',
+  },
+  {
+    name: ' r/dota2',
+    members: '100,000',
+    imgUrl: 'https://randomuser.me/api/portraits/men/65.jpg',
+  },
+  {
+    name: ' r/css',
+    members: '9,000',
+    imgUrl: 'https://randomuser.me/api/portraits/men/60.jpg',
+  },
+  {
+    name: ' r/tailwind',
+    members: '12,000',
+    imgUrl: 'https://randomuser.me/api/portraits/men/50.jpg',
+  },
+  {
+    name: ' r/tailwind',
+    members: '12,000',
+    imgUrl: 'https://randomuser.me/api/portraits/men/20.jpg',
+  },
 ];
 function Dropdown() {
+  const [toggleDropdown, setToggleDropdown] = useState(false);
+  const [postTarget, setPostTarget] = useState({ img: '', name: '' });
+
+  const handleSelectTargetPost = (url, name) => {
+    setPostTarget((cur) => ({ ...cur, img: url, name }));
+    setToggleDropdown((cur) => !cur);
+  };
   return (
     <div class="flex flex-col mt-5">
-      <div class="w-full md:w-1/2 flex flex-col items-center h-64">
+      <div class="max-w-xs w-full md:w-1/2 flex flex-col items-center h-64">
         <div class="w-full px-4">
           <div class="flex flex-col items-center relative">
             <div class="absolute shadow bg-white top-100 z-40 w-full lef-0 rounded max-h-select overflow-y-auto svelte-5uyqqj">
               {/* Input */}
               <div class="w-full">
-                <div class="my-2 p-1 bg-white flex border border-gray-200 rounded">
+                <div class="p-0.5 bg-white flex border border-gray-200 rounded">
                   <div class="flex flex-auto flex-wrap justify-center items-center">
-                    <img
-                      className="rounded-full w-7 h-7 m-2"
-                      alt="A"
-                      src="https://randomuser.me/api/portraits/men/62.jpg"
-                    />
+                    {postTarget.img ? (
+                      <img
+                        className="rounded-full w-6 h-6 mx-2"
+                        alt="A"
+                        src={postTarget.img}
+                      />
+                    ) : (
+                      <HiOutlineUserCircle fontSize="1.5em" />
+                    )}
                   </div>
                   <input
                     placeholder="Choose a community"
-                    className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
+                    className="p-1 px-2 appearance-none outline-none w-full text-gray-800 font-medium"
+                    value={postTarget.name}
                   />
-                  <div class="text-gray-300 w-8 py-1 pl-2 pr-1 border-l flex items-center border-gray-200">
+                  <div
+                    class="text-gray-300 w-8 py-1 pl-2 pr-1 border-l flex items-center border-gray-200"
+                    onClick={() => setToggleDropdown((cur) => !cur)}
+                  >
                     <button class="cursor-pointer w-6 h-6 text-gray-600 outline-none focus:outline-none">
                       <HiChevronDown />
                     </button>
@@ -40,47 +75,63 @@ function Dropdown() {
               </div>
 
               {/* Lists */}
-              <div class="flex flex-col w-full">
-                <div class="cursor-pointer w-full border-gray-100 rounded-t border-b hover:bg-teal-100">
-                  <div className="flex justify-between  items-center mb-2 ml-5">
-                    <div className="text-xs font-bold text-gray-500">
-                      My Profile
-                    </div>
-                  </div>
+              {toggleDropdown && (
+                <>
+                  <div className="flex flex-col w-full">
+                    <div className="cursor-pointer w-full border-gray-100 rounded-t border-b hover:bg-teal-100">
+                      <div className="flex justify-between  items-center mt-2 ml-5">
+                        <div className="text-xs font-bold text-gray-500">
+                          My Profile
+                        </div>
+                      </div>
 
-                  <div class="flex w-full items-center p-2 pl-2 border-transparent border-b border-gray-300 relative hover:border-teal-100 ">
-                    <div class="w-6 flex flex-col items-center mx-3">
-                      <div class="flex relative w-7 h-7 bg-orange-500 justify-center items-center m-1 mr-3  mt-1 rounded-full">
-                        <img
-                          class="rounded-full"
-                          alt="A"
-                          src="https://randomuser.me/api/portraits/men/62.jpg"
+                      <div
+                        className="flex w-full items-center p-2 pl-2 border-transparent border-b border-gray-300 relative hover:bg-gray-100"
+                        onClick={() =>
+                          handleSelectTargetPost(
+                            'https://randomuser.me/api/portraits/men/62.jpg',
+                            'u/Content_Avatar007'
+                          )
+                        }
+                      >
+                        <div className="w-6 flex flex-col items-center mx-3">
+                          <div className="flex relative w-7 h-7 bg-orange-500 justify-center items-center m-1 mr-3  mt-1 rounded-full">
+                            <img
+                              className="rounded-full"
+                              alt="A"
+                              src="https://randomuser.me/api/portraits/men/62.jpg"
+                            />
+                          </div>
+                        </div>
+                        <div className="w-full items-start flex flex-col ">
+                          <div className="text-sm truncate w-full normal-case font-semibold -mt-1 text-black ">
+                            u/Content_Avatar007
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="flex justify-between  items-center mb-2 ml-5 mt-2">
+                        <div className="text-xs font-bold text-gray-500">
+                          MY COMMUNITIES
+                        </div>
+                        <Link
+                          to={'/'}
+                          className="rounded-full text-xs font-bold  text-blue-500  px-4 py-1   transition duration-300 ease-in-out hover:bg-gray-300 "
+                        >
+                          Create New
+                        </Link>
+                      </div>
+                      {MOCK_DATA.map((item, idx) => (
+                        <DropdownCard
+                          key={idx}
+                          item={item}
+                          handleSelectTargetPost={handleSelectTargetPost}
                         />
-                      </div>
-                    </div>
-                    <div class="w-full items-start flex flex-col">
-                      <div class="text-sm truncate w-full normal-case font-semibold -mt-1 text-black">
-                        u/Content_Avatar007
-                      </div>
+                      ))}
                     </div>
                   </div>
-
-                  <div className="flex justify-between  items-center mb-2 ml-5 mt-2">
-                    <div className="text-xs font-bold text-gray-500">
-                      MY COMMUNITIES
-                    </div>
-                    <Link
-                      to={'/'}
-                      className="rounded-full text-xs font-bold  text-blue-500  px-4 py-1   transition duration-300 ease-in-out hover:bg-gray-300 mr-6"
-                    >
-                      Create New
-                    </Link>
-                  </div>
-                  {MOCK_DATA.map((item, idx) => (
-                    <DropdownCard key={idx} item={item} />
-                  ))}
-                </div>
-              </div>
+                </>
+              )}
             </div>
           </div>
         </div>
