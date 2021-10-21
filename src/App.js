7d8641a794ed1e4cadd647c6f1bb4d22969d6a0f
Tony <alessyraffaello@gmail.com>
2021-10-21 21:09:16 +0700

    Create main page for news feed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import Navbar from "./components/Navbar/Navbar";
-import FeedBox from "./components/newsFeed/FeedBox";
-import Popular from "./components/newsFeed/Popular";
-import SideCard from "./components/newsFeed/SideCard";
-import CreatePostAndCommunity from "./components/newsFeed/CreatePostAndCommunity";
-import Footer from "./components/Footer/Footer";
+import React from "react";
+import Main from "./components/newsFeed/MainPage/Main";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <div className="container">
-        <div className="sideCard">
-          <SideCard />
-          <br />
-          <CreatePostAndCommunity />
-          <br />
-          <Footer />
-          <br />
-        </div>
-        <Popular />
-        <FeedBox />
-        <FeedBox />
-        <FeedBox />
-      </div>
+      <Main />
     </div>
   );
 }
