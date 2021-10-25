357efc8020af1ab1da09268a2599f75a5a710f8b
akkpol <g.sepiro@gmail.com>
2021-10-25 11:31:21 +0700

    update PonstingOurweb

---
+++ src/component/PostingtoOurweb.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function PostingtoOurweb() {
+  return (
+    <div className="w-72 bg-gray-50 flex justify-center rounded-lg shadow-md">
+      <div className="w-11/12">
+        {" "}
+        <p className="mt-5 font-semibold">Posting to Ourweb</p>
+        <div class="my-1 grid grid-cols-1 divide-y divide-gray-200  ">
+          <div className="my-2 ">
+            <p className="mt-8">1.remember the humen</p>
+          </div>
+          <div className="my-2 ">
+            {" "}
+            <p className="mt-8">1.remember the humen</p>
+          </div>
+          <div className="my-2 ">
+            {" "}
+            <p className="mt-8">1.remember the humen</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
