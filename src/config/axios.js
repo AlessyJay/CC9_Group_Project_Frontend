95f0b4365a66d90dc7b950b0a09330ed8ccc9043
thalewnet <lewgasaemsant.t@gmail.com>
2021-10-28 10:54:36 +0700

    'dasdas'

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,28 +1,26 @@
-import axios from "axios";
-import { API_URL } from "./env";
-import { createBrowserHistory } from "history";
-import { getToken, removeToken } from "../services/localStorage";
+import axios from 'axios';
+import { API_URL } from './env';
+import { getToken, removeToken } from '../services/localStorage';
+import { history } from '../index';
 
 axios.defaults.baseURL = API_URL;
 
-const history = createBrowserHistory();
-
 axios.interceptors.request.use(
-  config => {
+  (config) => {
     config.headers.authorization = `Bearer ${getToken()}`;
     return config;
   },
-  err => Promise.reject.err
+  (err) => Promise.reject.err
 );
 
 axios.interceptors.response.use(
-  response => {
+  (response) => {
     return response;
   },
-  err => {
+  (err) => {
     if (err.response && err.response.status === 401) {
       removeToken();
-      history.push({ pathname: "/", state: true });
+      history.push({ pathname: '/', state: true });
       window.location.reload();
       // window.location.replace("/");
       return;
@@ -30,4 +28,5 @@ axios.interceptors.response.use(
     return Promise.reject(err);
   }
 );
+
 export default axios;
