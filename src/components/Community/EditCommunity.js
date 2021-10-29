ceb3d4641c7061566a669d3e450aab0ff6247a0b
paramee.1 <huhaliza@gmail.com>
2021-10-29 15:02:55 +0700

    merge

--- src/components/Community/EditCommunity.js
+++ src/components/Community/EditCommunity.js
@@ -174,10 +174,7 @@ export default function EditCommunity() {
             </div>
             <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
               {(descriptions || showProfile) && (
-                <button
-                  type="button"
-                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                >
+                <button className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                   Submit
                 </button>
               )}
