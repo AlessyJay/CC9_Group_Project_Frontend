6a7918430844dac3dd2a4e7b61bc35f7169363d3
paramee.1 <huhaliza@gmail.com>
2021-10-30 15:57:42 +0700

    edit page community

--- src/pages/EditCommunitPage.js
+++ src/pages/EditCommunitPage.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import CommunityHeader from '../components/Community/CommunityHeader';
-import Footer from '../components/Footer/Footer';
-import EditCommunity from '../components/Community/EditCommunity';
+import React from "react";
+import CommunityHeader from "../components/Community/CommunityHeader";
+import Footer from "../components/Footer/Footer";
+import EditCommunity from "../components/Community/EditCommunity";
 
 function EditCommunitPage() {
   return (
     <div className="bg-gray-200">
-      <CommunityHeader />
       <div className=" grid grid-cols-7 gap-6">
         <div className="col-start-2 col-span-3 ">
           <EditCommunity />
